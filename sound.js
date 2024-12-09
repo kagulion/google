016591ac9d32de9a746dda2083bbaae58a14bd1e
KAGU <kagulion@vk.com>
2024-12-09 18:51:01 +0300

    0.0.5

---
+++ sound.js
@@ -0,0 +1,9 @@
+// Поиск
+document.getElementById('main__search-button').addEventListener('click', function() {
+  document.getElementById('ass').play();
+});
+
+// Лаки
+document.getElementById('main__lucky-button').addEventListener('click', function() {
+  document.getElementById('anal').play();
+});
